8da7689598a7a21a943a686d834067851b09afc8
Maria Ardila <75107876+mfardilam@users.noreply.github.com>
2021-11-23 22:28:10 -0500

    Login y parte de productos

---
+++ Login y productos/Proyecto/src/keys.js
@@ -0,0 +1,9 @@
+//para exportar configuraciones, aca le dare la ubicacion de la BD
+
+module.exports = {
+    mongodb: {
+        URI: 'mongodb://localhost:27017/TiendaAlem'
+    }
+
+};
+//No servi
