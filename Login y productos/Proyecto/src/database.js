0e2631e6960f0cb337204109bbd742675c2c20bb
cristian1767 <90486186+cristian1767@users.noreply.github.com>
2021-11-27 23:05:12 -0500

    Delete Login y productos/Proyecto directory

--- Login y productos/Proyecto/src/database.js
+++
@@ -1,6 +0,0 @@
-//para conectarnos a la BD
-const mongoose = require('mongoose');
-
-//conexion
-mongoose.connect('mongodb://localhost:27017/TiendaAlem')
-.then(db=> console.log("Conecte bien c:"));
--- Login y productos/Proyecto/src/keys.js
+++
@@ -1,9 +0,0 @@
-//para exportar configuraciones, aca le dare la ubicacion de la BD
-
-module.exports = {
-    mongodb: {
-        URI: 'mongodb://localhost:27017/TiendaAlem'
-    }
-
-};
-//No servi
